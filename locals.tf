327ad5ad8b359f61c72d73d1e66c8ed07d82aed3
Gerson <gersonrodriguessantos8@gmail.com>
2025-02-28 10:57:56 -0300

    feat: add kiali

--- locals.tf
+++ locals.tf
@@ -6,5 +6,31 @@ locals {
     cni = {
       profile = "ambient"
     }
+    kiali-operator = {
+      cr = {
+        create = true
+        namespace : var.namespace
+        annotations : {}
+        spec = {
+          auth = {
+            strategy = "anonymous"
+          }
+          deployment = {
+            cluster_wide_access = false
+            discovery_selectors = {
+              default = [{
+                matchLabels = {
+                  "kubernetes.io/metadata.name" = "bookinfo"
+                }
+              }]
+            }
+            view_only_mode = false
+          }
+          server = {
+            web_root = "/kiali"
+          }
+        }
+      }
+    }
   }]
 }
