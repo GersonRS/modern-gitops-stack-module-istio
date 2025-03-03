298097183ae5a4f55c62e67c8bd0258bf4230702
Gerson <gersonrodriguessantos8@gmail.com>
2025-03-03 13:04:34 -0300

    feat: config kiali

--- locals.tf
+++ locals.tf
@@ -16,18 +16,75 @@ locals {
             strategy = "anonymous"
           }
           deployment = {
-            cluster_wide_access = false
-            discovery_selectors = {
-              default = [{
-                matchLabels = {
-                  "kubernetes.io/metadata.name" = "bookinfo"
-                }
-              }]
-            }
+            cluster_wide_access = true
+            # discovery_selectors = {
+            #   default = [
+            #     {
+            #       matchLabels = {
+            #         "kubernetes.io/metadata.name" = "default"
+            #       }
+            #     },
+            #     {
+            #       matchLabels = {
+            #         region = "east"
+            #       },
+            #       matchExpressions = [
+            #         {
+            #           key      = "app"
+            #           operator = "In"
+            #           values   = ["ticketing"]
+            #         },
+            #         {
+            #           key      = "color"
+            #           operator = "In"
+            #           values   = ["blue"]
+            #         }
+            #       ]
+            #     }
+            #   ]
+            # }
             view_only_mode = false
           }
           server = {
             web_root = "/kiali"
+          }
+          external_services = {
+            prometheus = {
+              url = "http://kube-prometheus-stack-prometheus.kube-prometheus-stack.svc.cluster.local:9090/"
+            }
+            grafana = {
+              enabled      = true
+              internal_url = "http://kube-prometheus-stack-grafana.kube-prometheus-stack.svc.cluster.local:3000/"
+              external_url = "https://grafana.apps.kind.172-19-0-101.nip.io/"
+              dashboards = [
+                {
+                  name = "Istio Service Dashboard"
+                  variables = {
+                    namespace = var.namespace
+                    service   = "var-service"
+                  }
+                },
+                {
+                  name = "Istio Workload Dashboard"
+                  variables = {
+                    namespace = var.namespace
+                    workload  = "var-workload"
+                  }
+                },
+                {
+                  name = "Istio Mesh Dashboard"
+                },
+                {
+                  name = "Istio Control Plane Dashboard"
+                },
+                {
+                  name = "Istio Performance Dashboard"
+                },
+                {
+                  name = "Istio Wasm Extension Dashboard"
+                }
+              ]
+            }
           }
         }
       }
